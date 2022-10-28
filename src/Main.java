dbeddd595e158d43e8ac5e60bfddb0169ec2a402
glaucoserrano <glaucoserrano@gmail.com>
2022-10-28 11:41:02 -0300

    Desafio Concluido

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -15,7 +17,25 @@ public class Main {
         mentoria.setTitulo("Mentoria Java");
         mentoria.setdata(LocalDate.now());
 
-        System.out.println((curso));
-        System.out.println((mentoria));
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do bootcamp");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Glauco Serrano");
+        dev.inscreverBootcamp(bootcamp);
+
+        System.out.println(("Conteudos Inscrito: " + dev.getConteudosInscritos()));
+        dev.progredir();
+        System.out.println(("Conteudos Concluidos: " + dev.getConteudosConcluidos()));
+        System.out.println(("XP: " + dev.calcularTotalXP()));
+        dev.progredir();
+        System.out.println(("Conteudos Concluidos: " + dev.getConteudosConcluidos()));
+        System.out.println(("XP: " + dev.calcularTotalXP()));
+        System.out.println(("Conteudos Inscrito: " + dev.getConteudosInscritos()));
+        // System.out.println((curso));
+        // System.out.println((mentoria));
     }
 }
